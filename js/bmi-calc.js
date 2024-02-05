8467e8105c1f200f500988e9ef6ece5580ee3ad8
dwisign <dwisign.mail@gmail.com>
2024-02-05 14:41:00 +0700

    update BMI calculator

--- js/bmi-calc.js
+++ js/bmi-calc.js
@@ -6,5 +6,18 @@ function bmiCalc(){
 
     bmiCalcCount = (weight / Math.pow((height/100),2)).toFixed(2)
     document.getElementById('resultBmi').textContent = bmiCalcCount
+
+    if(bmiCalcCount < 18.5){
+        indicator = "Underweight"
+    }else if(bmiCalcCount >= 18.5 && bmiCalcCount <= 24.9){
+        indicator = "Normal Weight"
+    }else if(bmiCalcCount >= 25 && bmiCalcCount <= 29.9){
+        indicator = "Over Weight"
+    }else{
+        indicator = "Obese"
+    }
+    
+    console.log('TEST', indicator)
+    document.getElementById('resultIndicator').textContent = indicator
     
 }
